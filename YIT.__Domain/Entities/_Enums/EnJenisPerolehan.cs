88ab03c1740029923c52504fd0e4167249c70ccb
nisa <112600657+wk661@users.noreply.github.com>
2024-09-03 12:00:02 +0800

    Update the "jenis" field: set 0 to "Empty" and add 99 for "semua".

--- YIT.__Domain/Entities/_Enums/EnJenisPerolehan.cs
+++ YIT.__Domain/Entities/_Enums/EnJenisPerolehan.cs
@@ -4,18 +4,21 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace YIT.__Domain.Entities._Enums
 {
     public enum EnJenisPerolehan
     {
-        [Display(Name = "Semua")]
-        Semua = 0,
+        [Display(Name = "")]
+        None = 0,
         [Display(Name = "Bekalan")]
         Bekalan = 1,
         [Display(Name = "Perkhidmatan")]
         Perkhidmatan = 2,
         [Display(Name = "Kerja")]
-        Kerja = 3
+        Kerja = 3,
+        [Display(Name = "Semua")]
+        Semua = 99,
     }
 }
