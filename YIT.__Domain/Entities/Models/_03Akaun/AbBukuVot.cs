0ca3a52a385509067d39630c769820561387a01c
Saidi Daud <saididaud@gmail.com>
2024-11-08 15:17:58 +0800

    saidi-Revert-AbbukuVot

--- YIT.__Domain/Entities/Models/_03Akaun/AbBukuVot.cs
+++ YIT.__Domain/Entities/Models/_03Akaun/AbBukuVot.cs
@@ -27,8 +27,6 @@ namespace YIT.__Domain.Entities.Models._03Akaun
         public DateTime Tarikh { get; set; }
         public int? DDaftarAwamId { get; set; }
         public DDaftarAwam? DDaftarAwam { get; set; }
-        public int? DPekerjaId { get; set; }
-        public DPekerja? DPekerja { get; set; }
         [DisplayName("Vot")]
         public int VotId { get; set; }
         public AkCarta? Vot { get; set; }
