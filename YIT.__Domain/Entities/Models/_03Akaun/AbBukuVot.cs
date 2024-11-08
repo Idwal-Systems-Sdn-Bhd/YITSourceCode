3955c4329e8151e2ac918606528144cc7e9a945e
Saidi Daud <saididaud@gmail.com>
2024-11-08 09:47:58 +0800

    saidi Remove AbBukuVot Perihal

--- YIT.__Domain/Entities/Models/_03Akaun/AbBukuVot.cs
+++ YIT.__Domain/Entities/Models/_03Akaun/AbBukuVot.cs
@@ -29,7 +29,6 @@ namespace YIT.__Domain.Entities.Models._03Akaun
         public DDaftarAwam? DDaftarAwam { get; set; }
         public int? DPekerjaId { get; set; }
         public DPekerja? DPekerja { get; set; }
-        public string? Perihal { get; set; }
         [DisplayName("Vot")]
         public int VotId { get; set; }
         public AkCarta? Vot { get; set; }
